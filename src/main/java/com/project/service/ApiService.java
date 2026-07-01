38ec23b3dfe06e6d72ec3ad868f1ffb32faf3418
manigandanbabutg-new <manigandanbabu.tg@gmail.com>
2026-07-01 07:21:01 +0530

    Update ApiService.java

---
+++ src/main/java/com/project/service/ApiService.java
@@ -0,0 +1,12 @@
+package com.project.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApiService {
+
+    public String getMessage() {
+        return "Hello from Spring Boot!";
+    }
+
+}
