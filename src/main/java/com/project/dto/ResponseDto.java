df65083f50435531ff8a2d8286fa894a98aba99f
manigandanbabutg-new <manigandanbabu.tg@gmail.com>
2026-07-01 07:22:08 +0530

    Update ResponseDto.java

---
+++ src/main/java/com/project/dto/ResponseDto.java
@@ -0,0 +1,22 @@
+package com.project.dto;
+
+public class ResponseDto {
+
+    private String response;
+
+    public ResponseDto() {
+    }
+
+    public ResponseDto(String response) {
+        this.response = response;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+}
