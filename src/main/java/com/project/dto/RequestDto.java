5aa2d5a70d002186da10a270b2f5e5c9e80f794e
manigandanbabutg-new <manigandanbabu.tg@gmail.com>
2026-07-01 07:21:34 +0530

    Update RequestDto.java

---
+++ src/main/java/com/project/dto/RequestDto.java
@@ -0,0 +1,22 @@
+package com.project.dto;
+
+public class RequestDto {
+
+    private String message;
+
+    public RequestDto() {
+    }
+
+    public RequestDto(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
